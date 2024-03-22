ea04d460ac8af59e871b0d4c7af22669f1576b34
alexpr42 <8988@holbertonstudents.com>
2024-03-21 23:10:16 -0400

    get_fun.c fix

--- get_fun.c
+++ get_fun.c
@@ -30,36 +30,3 @@ int get_fun(char c, va_list *vlist)
 	_putchar(c);
 	return (2);
 }
-
-/**
- * print_string - Imprime una cadena de caracteres
- * @vlist: Lista de argumentos variable
- *
- * Return: La cantidad de caracteres impresos
- */
-int print_string(va_list *vlist)
-{
-    return (accString(vlist));
-}
-
-/**
- * _putchar - print char in stdout
- * @c: the char
- *
- * Return: print 1 if is correct
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-/**
- * print_int - print number
- * @vlist: list of args
- *
- * Return: amount of digits
- */
-int print_int(va_list *vlist)
-{
-    return (accInt(vlist));
-}
