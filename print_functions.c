0618356701f7aa303b779dea15eff487172b44e5
alexpr42 <8988@holbertonstudents.com>
2024-03-21 23:18:44 -0400

    print_functions.c fix

--- print_functions.c
+++ print_functions.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_string - print char
+ * @str: print chain of char
+ *
+ * Return: amount of char printed
+ */
 
 int print_string(char *str)
 {
@@ -15,12 +21,24 @@ int print_string(char *str)
 	return (i);
 }
 
+/**
+ * _putchar - print char to stdout
+ * @c: char
+ *
+ * Return: 1 if printed correctly
+ */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
+/**
+ * print_int - print numbers
+ * @num: number to print
+ *
+ * Return: amount of digits printed
+ */
 
 int print_int(int num)
 {
