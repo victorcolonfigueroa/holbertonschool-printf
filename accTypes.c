ad64697a20a1a120a3f690e28db27d4215293084
alexpr42 <8988@holbertonstudents.com>
2024-03-21 22:52:39 -0400

    accTypes.c fix

--- accTypes.c
+++ accTypes.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * accString - string
- * accChar - charracter
- * accInt - integer
- * accPercent - percerts
+ * accString - print strings
  * @vlist: list v
  *
- * return: total (1)
+ * return: total charracters printed
  */
 
 int accString(va_list *vlist)
@@ -15,12 +12,24 @@ int accString(va_list *vlist)
 	return (print_string(va_arg(*vlist, char *)));
 }
 
+/**
+ * accChar - print char
+ * @vlist: list of args
+ *
+ * return: 1 if printed correctly
+ */
 
 int accChar(va_list *vlist)
 {
 	return (_putchar(va_arg(*vlist, int)));
 }
 
+/**
+ * accInt - print number
+ * @vlist: list of args
+ *
+ * return: amount of digits
+ */
 
 int accInt(va_list *vlist)
 {
